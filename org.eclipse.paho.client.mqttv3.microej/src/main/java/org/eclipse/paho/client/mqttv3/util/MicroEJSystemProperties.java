6b62d87a69fa4dec8bf3e6c663ee45e8a8984fa8
Stanislas Kobak <skobak@is2t-intern.com>
2015-05-05 11:10:28 +0200

    Use appropriate copyright pattern.

--- org.eclipse.paho.client.mqttv3.microej/src/main/java/org/eclipse/paho/client/mqttv3/util/MicroEJSystemProperties.java
+++ org.eclipse.paho.client.mqttv3.microej/src/main/java/org/eclipse/paho/client/mqttv3/util/MicroEJSystemProperties.java
@@ -1,13 +1,19 @@
-/*
- * Java
+/*******************************************************************************
+ * Copyright (c) 2015 IS2T.
  *
- * Copyright 2015 IS2T. All rights reserved.
- * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution. 
+ *
+ * The Eclipse Public License is available at 
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
  */
+
 
 package org.eclipse.paho.client.mqttv3.util;
 
-import java.util.Properties;
 
 public class MicroEJSystemProperties implements SystemProperties {
 
